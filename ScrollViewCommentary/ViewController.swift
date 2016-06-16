f160eebaeabe1a386a90818b2a8f6b8ce30a46bf
Jonathon Staff <jon@nplexity.com>
2016-06-16 21:54:58 +0200

    Simple display of UIScrollView bounds.origin vs contentOffset

--- ScrollViewCommentary/ViewController.swift
+++ ScrollViewCommentary/ViewController.swift
@@ -10,16 +10,91 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  weak var scrollView: UIScrollView!
+  weak var boundsLabel: UILabel!
+  weak var contentOffsetLabel: UILabel!
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    view.translatesAutoresizingMaskIntoConstraints = false
+
+    scrollView = createScrollView()
+    boundsLabel = createBoundsLabel()
+    contentOffsetLabel = createContentOffsetLabel()
+
+    NSLayoutConstraint(item: boundsLabel,
+                       attribute: .Bottom,
+                       relatedBy: .Equal,
+                       toItem: contentOffsetLabel,
+                       attribute: .Top,
+                       multiplier: 1.0,
+                       constant: -8).active = true
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
+private extension ViewController {
+  func createScrollView() -> UIScrollView {
+    let scrollView = UIScrollView()
+    scrollView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+    scrollView.frame = CGRect(origin: .zero, size: view.bounds.size)
+    view.addSubview(scrollView)
+
+    let image = UIImage(named: "death-valley")
+    let imageView = UIImageView(image: image)
+    imageView.center.y = scrollView.center.y
+
+    scrollView.addSubview(imageView)
+    scrollView.contentSize.width = CGRectGetWidth(imageView.bounds)
+
+    scrollView.delegate = self
+    return scrollView
+  }
+
+  func createBoundsLabel() -> UILabel {
+    let label = UILabel()
+    label.textColor = .whiteColor()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(label)
+
+    NSLayoutConstraint(item: label,
+                       attribute: .Leading,
+                       relatedBy: .Equal,
+                       toItem: view,
+                       attribute: .Leading,
+                       multiplier: 1.0,
+                       constant: 8).active = true
+
+    return label
+  }
+
+  func createContentOffsetLabel() -> UILabel {
+    let label = UILabel()
+    label.textColor = .whiteColor()
+    label.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(label)
+
+    NSLayoutConstraint(item: label,
+                       attribute: .Bottom,
+                       relatedBy: .Equal,
+                       toItem: view,
+                       attribute: .Bottom,
+                       multiplier: 1.0,
+                       constant: -8).active = true
+    NSLayoutConstraint(item: label,
+                       attribute: .Leading,
+                       relatedBy: .Equal,
+                       toItem: view,
+                       attribute: .Leading,
+                       multiplier: 1.0,
+                       constant: 8).active = true
+
+    return label
+  }
+}
+
+extension ViewController: UIScrollViewDelegate {
+  func scrollViewDidScroll(scrollView: UIScrollView) {
+    boundsLabel.text = String(format: "origin.x (bounds): %.2f", scrollView.bounds.origin.x)
+    contentOffsetLabel.text = String(format: "contentOffset.x: %.2f", scrollView.contentOffset.x)
+  }
+}
