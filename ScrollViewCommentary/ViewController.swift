d346f00e56b894199759a61d534b9d537374a595
Jonathon Staff <jon@nplexity.com>
2016-06-16 19:08:54 +0200

    Initial Commit

---
+++ ScrollViewCommentary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollViewCommentary
+//
+//  Created by Jonathon Staff on 6/16/16.
+//  Copyright © 2016 Nplexity LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
